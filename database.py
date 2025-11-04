6803b5f69c23344b1faf1931340f4693904f91b9
eldablo23307 <dany.mottu@gmail.com>
2025-11-04 09:16:39 +0100

    Fix Registration Problem

--- database.py
+++ database.py
@@ -6,8 +6,9 @@ key = b'UF8ofzIpXvMP6gv0bOL-qyVCrvHEwoVdm9fRcLW8l74='
 fernet = Fernet(key)
 class database():
 
-    def add_user(nm: str, psw: str, filename: str):
+    def add_user(nm, psw: str, filename: str):
         db = TinyDB("./static/db.json")
+        nm = str(nm)
         nm = nm.replace(" ", "%")
         info = Query()
         id = fernet.encrypt(f"{nm}".encode())
@@ -17,8 +18,9 @@ class database():
         else:
             return False
     
-    def check_user(nm: str, psw: str):
+    def check_user(nm, psw: str):
         db = TinyDB("./static/db.json")
+        nm = str(nm)
         info = Query()
         nm = nm.replace(" ", "%")
         if db.search(info.name == nm) != [] and db.search(info.password == psw) != []:
@@ -26,7 +28,8 @@ class database():
         else:
             return False
 
-    def give_user_info(nm: str):
+    def give_user_info(nm):
+        nm = str(nm)
         db = TinyDB("./static/db.json")
         info = Query()
         nm = nm.replace(" ", "%")
@@ -35,9 +38,10 @@ class database():
             for record in data:
                 return [record["name"], record["filename"], record["id"]]
             
-    def give_user_info_with_id(id: str):
+    def give_user_info_with_id(id):
         db = TinyDB("./static/db.json")
         info = Query()
+        id = str(id)
         id = id.replace(" ", "%")
         data = db.search(info.id == id)
         if data:
@@ -45,8 +49,9 @@ class database():
                 return [record["name"], record["filename"], record["id"]]
     
 class database_post():
-    def add_post(name: str, post: str, img_profile: str):
+    def add_post(name, post: str, img_profile: str):
         post_db = TinyDB("./static/post.json")
+        name = str(name)
         img_profile = img_profile.replace("%", " ")
         name = name.replace("%", " ")
         post = profanity.censor(post)
